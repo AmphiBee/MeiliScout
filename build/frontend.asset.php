7e76a1a45d3bc52cfe1666b0277d1e5771f83634
Olivier Gorzalka <olivier@amphibee.fr>
2025-08-04 17:19:36 +0200

    Added missing build

---
+++ build/frontend.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'b19a32c89b944258f5b9');
---
+++ build/main.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'dfce3f36855139e4afd0');
